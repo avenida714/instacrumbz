ecc1965b7d6e7b0114fa7746fa336c3861efc5d5
Alec Venida <alecvenida@gmail.com>
2022-09-16 16:51:36 -0700

    comment forms

--- app/forms/comment_form.py
+++ app/forms/comment_form.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import validators
 from wtforms.validators import DataRequired, url
 from wtforms.fields import (
-    BooleanField, SelectField, DateField, StringField, PasswordField, SubmitField, TextAreaField, TimeField, IntegerField, TextAreaField
+    BooleanField, SelectField, DateField, StringField, PasswordField, SubmitField, TextAreaField, TimeField, IntegerField
 )
 
 
 class CommentForm(FlaskForm):
-    comment = TextAreaField("Comment")
+    comment = TextAreaField("Comment", validators=[DataRequired(),
+        validators.Length( min=1, max=2000, message='Comment must be between 1 and 2000 characters')])

7ac9c316fe55f7bab792b69bc1ff4d5311b44a36
Alec Venida <alecvenida@gmail.com>
2022-09-16 16:27:10 -0700

    heroku test

--- app/__init__.py
+++ app/__init__.py
@@ -42,7 +42,7 @@ CORS(app)
 # we won't be using a buildpack when we deploy to Heroku.
 # Therefore, we need to make sure that in production any
 # request made over http is redirected to https.
-# Well.........
+# Well.............
 @app.before_request
 def https_redirect():
     if os.environ.get('FLASK_ENV') == 'production':

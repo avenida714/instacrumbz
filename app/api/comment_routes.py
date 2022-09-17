25bf6969c48972a763aa5978b4326dcdbfd3cc2b
RayC206 <rcv.henry@gmail.com>
2022-09-17 14:01:28 -0700

    working on profile route, not working on comments crud anymore

--- app/api/comment_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from app.models import db, User, Post, Comment
-
-
-comments_route = Blueprint("comments", __name__, url_prefix='/comments')
-
-@comments_route.route("/<comment_id>")
-def edit_comment():
-  pass

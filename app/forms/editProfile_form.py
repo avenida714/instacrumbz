64d576098c7530a8d662fffeff71752c6980989f
RayC206 <rcv.henry@gmail.com>
2022-09-16 18:42:08 -0700

    edit profile form

---
+++ app/forms/editProfile_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+class editProfileForm(FlaskForm):
+  username = StringField("Username", validators=[DataRequired()])
+  bio = StringField("Bio")
+  email = StringField("Email", validators=[DataRequired()])
+  gender = StringField("Gender")
+  profile_img = StringField("Profile Image URL")

64c5047f5d2d1865ba8bdb22cf6ed667335453d7
Rudy Nguyen <rudyn2010@gmail.com>
2022-09-18 09:06:16 -0700

    linked template reducer to main store

--- react-app/src/store/posts.js
+++ react-app/src/store/posts.js
@@ -1,9 +1,9 @@
 //Types:
 const CREATE_POST = 'posts/createPost';
-const READ_POST = 'posts/readSpot';
-const UPDATE_POST = 'posts/updateSpot';
-const DELETE_POST = 'posts/deleteSpot';
-const LOAD_ALL_SPOTS = 'posts/loadSpots'; //might not need this?
+const READ_POST = 'posts/readPost';
+const UPDATE_POST = 'posts/updatePost';
+const DELETE_POST = 'posts/deletePost';
+const LOAD_ALL_POSTS = 'posts/loadPosts'; //might not need this?
 
 
 //Action Creators:
@@ -33,7 +33,7 @@ const actionDeletePost = () => {
 }
 const actionLoadPosts = () => {
     return {
-        type: LOAD_ALL_SPOTS,
+        type: LOAD_ALL_POSTS,
         //payload here
     }
 }
@@ -69,9 +69,24 @@ const initialState = {};
 //Reducer:
 const postsReducer = (state = initialState, action) => {
     switch (action.type) {
+        case CREATE_POST: {
+            return
+        }
+        case READ_POST: {
+            return
+        }
+        case UPDATE_POST: {
+            return
+        }
+        case DELETE_POST: {
+            return
+        }
+        case LOAD_ALL_POSTS: {
+            return
+        }
         default:
             return state;
-    }
-}
+    };
+};
 
 export default postsReducer;

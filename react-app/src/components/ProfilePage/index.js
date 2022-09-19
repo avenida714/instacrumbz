c17b077fdfc12540bdb22fbff782c7fbe98d4e5c
RayC206 <rcv.henry@gmail.com>
2022-09-19 11:54:23 -0700

    added conditional for non existing user profile and user posts

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -1,78 +1,81 @@
-import React, {useEffect, useState} from 'react';
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { loadUserProfile } from '../../store/profile';
-import "./ProfilePage.css"
+import { loadUserProfile } from "../../store/profile";
+import "./ProfilePage.css";
 
 const UserProfilePage = () => {
   let { userId } = useParams();
   userId = Number(userId);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const userPosts = useSelector((state) => state.profile.posts);
   const profile = useSelector((state) => state.profile.profile);
   const [findAProfileStatus, setFindAProfileStatus] = useState(200);
   const [isLoaded, setIsLoaded] = useState(false);
-  console.log("here=====",userPosts)
+  console.log("here=====", userPosts);
 
   useEffect(() => {
     dispatch(loadUserProfile(userId))
-    .then(() => {
-      setIsLoaded(true);
-    })
-    .catch(async (res) => {
-      setFindAProfileStatus(res.status);
-    });
-  }, [dispatch])
+      .then(() => {
+        setIsLoaded(true);
+      })
+      .catch(async (res) => {
+        setFindAProfileStatus(res.status);
+      });
+  }, [dispatch]);
 
   if (isLoaded) {
     if (findAProfileStatus === 200) {
       return (
-        <div className='mainProfileContainer'>
-          <div className='innerProfileContainer'>
-              {profile.map((profile)=>{
-                if (profile) {
-                  return(
-                    <div className='profileInfoContainer' key={profile.id}>
-                      <div className='profilePic'><img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png'></img></div>
-                      <div className='profileDetails'>
-                        <h1>{profile.name}</h1>
-                        <span>{userPosts.length}</span> posts
-                        <span>{profile.followers.length}</span> followers
-                        <span> {profile.following.length} </span> following
-                        <div>{profile.bio}</div>
-                      </div>
+        <div className="mainProfileContainer">
+          <div className="innerProfileContainer">
+            {profile ? (
+              profile.map((profile) => {
+                return (
+                  <div className="profileInfoContainer" key={profile.id}>
+                    <div className="profilePic">
+                      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png"></img>
                     </div>
-                  )
-                } else {
-                  <div>404</div>
-                }
-              })}
+                    <div className="profileDetails">
+                      <h2>{profile.name}</h2>
+                      <span>{userPosts.length}</span> posts
+                      <span> {profile.followers.length}</span> followers
+                      <span> {profile.following.length} </span> following
+                      <div className="profileBio">{profile.bio}</div>
+                    </div>
+                  </div>
+                );
+              })
+            ) : (
+              <div>Profile not found</div>
+            )}
           </div>
-          <div>------------------</div>
-          <div className='userPostContainer'>
-              {userPosts.map((posts)=>{
-                if (posts) {
-                  return (
-                    <div className='eachUserPost' key={posts.id} >
-                      <div>{posts.caption}</div>
-                    </div>
-                  )
-                }
-              })}
+          
+          <div className="userPostContainer">
+            {userPosts && userPosts.length ? (
+              userPosts.map((posts) => {
+                return (
+                  <div className="eachUserPost" key={posts.id}>
+                    <div>{posts.caption}</div>
+                  </div>
+                );
+              })
+            ) : (
+              <div>no posts</div>
+            )}
           </div>
         </div>
-      )
+      );
     } else if (findAProfileStatus === 404) {
-        return (
-          <div className="notFound">
-            <span className="notFoundError">404: not found</span>
-          </div>
-        );
-      }
-    } else {
-      return <div>Loading... </div>;
+      return (
+        <div className="notFound">
+          <span className="notFoundError">404: not found</span>
+        </div>
+      );
     }
-  };
-
+  } else {
+    return <div>Loading... </div>;
+  }
+};
 
 export default UserProfilePage;

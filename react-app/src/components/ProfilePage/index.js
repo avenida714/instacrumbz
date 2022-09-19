4f1174e29630200a639c7b08c9911598804fb3dc
RayC206 <rcv.henry@gmail.com>
2022-09-19 11:31:17 -0700

    working on getting conditional for non existing profiles to work

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -10,6 +10,7 @@ const UserProfilePage = () => {
   const dispatch = useDispatch()
   const userPosts = useSelector((state) => state.profile.posts);
   const profile = useSelector((state) => state.profile.profile);
+  const [findAProfileStatus, setFindAProfileStatus] = useState(200);
   const [isLoaded, setIsLoaded] = useState(false);
   console.log("here=====",userPosts)
 
@@ -18,42 +19,60 @@ const UserProfilePage = () => {
     .then(() => {
       setIsLoaded(true);
     })
+    .catch(async (res) => {
+      setFindAProfileStatus(res.status);
+    });
   }, [dispatch])
 
   if (isLoaded) {
-    return (
-      <div className='mainProfileContainer'>
-        <div className='innerProfileContainer'>
-            {profile.map((profile)=>{
-              return(
-                <div className='profileInfoContainer' key={profile.id}>
-                  <div className='profilePic'><img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png'></img></div>
-                  <div className='profileDetails'>
-                    <h1>{profile.name}</h1>
-                    <span>{userPosts.length} posts </span>
-                    <span>{profile.followers.length} followers</span>
-                    <span> {profile.following.length} following</span>
-                    <div>{profile.bio}</div>
-                  </div>
-                </div>
-              )
-            })}
+    if (findAProfileStatus === 200) {
+      return (
+        <div className='mainProfileContainer'>
+          <div className='innerProfileContainer'>
+              {profile.map((profile)=>{
+                if (profile) {
+                  return(
+                    <div className='profileInfoContainer' key={profile.id}>
+                      <div className='profilePic'><img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png'></img></div>
+                      <div className='profileDetails'>
+                        <h1>{profile.name}</h1>
+                        <span>{userPosts.length}</span> posts
+                        <span>{profile.followers.length}</span> followers
+                        <span> {profile.following.length} </span> following
+                        <div>{profile.bio}</div>
+                      </div>
+                    </div>
+                  )
+                } else {
+                  <div>404</div>
+                }
+              })}
+          </div>
+          <div>------------------</div>
+          <div className='userPostContainer'>
+              {userPosts.map((posts)=>{
+                if (posts) {
+                  return (
+                    <div className='eachUserPost' key={posts.id} >
+                      <div>{posts.caption}</div>
+                    </div>
+                  )
+                }
+              })}
+          </div>
         </div>
-        <div>------------------</div>
-        <div className='userPostContainer'>
-            {userPosts.map((posts)=>{
-              return (
-                <div className='eachUserPost' key={posts.id} >
-                  <div>{posts.caption}</div>
-                </div>
-              )
-            })}
-        </div>
-      </div>
-    )
-  } else {
-    return <div>Loading ... </div>
-  }
-}
+      )
+    } else if (findAProfileStatus === 404) {
+        return (
+          <div className="notFound">
+            <span className="notFoundError">404: not found</span>
+          </div>
+        );
+      }
+    } else {
+      return <div>Loading... </div>;
+    }
+  };
+
 
 export default UserProfilePage;

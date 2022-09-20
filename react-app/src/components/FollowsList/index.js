fd1df72e7468d09cf4d92abc9a62180c8432e378
Rudy Nguyen <rudyn2010@gmail.com>
2022-09-19 17:04:55 -0700

    add main post page and follows templates

---
+++ react-app/src/components/FollowsList/index.js
@@ -0,0 +1,13 @@
+import './FollowsList.css'
+
+
+const FollowsList = ({ currUser }) => {
+
+    return (
+        <div className='follows-outer-div'>
+            <div>Testing Some</div>
+        </div>
+    )
+};
+
+export default FollowsList;
--- react-app/src/components/PostCard/index.js
+++ react-app/src/components/PostCard/index.js
@@ -1,32 +1,36 @@
+import { TiHeartOutline } from "react-icons/ti";
+// import { useSelector } from "react-redux";
 import "./PostCard.css"
+
 
 //TO-DO: Rudy finish Post Card
 const PostCard = ({ post }) => {
 
     return (
         <div className="outter-div-pc" /* outter main div container for single post */ >
-            <div className="" /* profile image / name */ >
-                <div>{/* user profile image */}</div>
-                <p>{ post.owner_id }</p>
+            <div className="header-pc" >
+                <div className="user-icon-pc">
+                    <img className="user-img" src={ post.user.profile_img }/>
+                </div>
+                <div className="header-info">
+                    <div>{ post.user.username }</div>
+                    <div>{ post.location }</div>
+                </div>
             </div>
             <div className="post-card-img-container" /* image display container */ >
                 <img className="post-card-img" src={ post?.image_url } />
             </div>
             <div>
-                <button>Like Button</button>
-                <button>Comment Button</button>
+                <TiHeartOutline className="heart-pc" />
             </div>
-            <div className="" /* caption container */ >
-                <p>{ post.caption }</p>
+            <div className="caption-pc" /* caption container */ >
+                <div>{ post.user.username }</div>
+                <div>{ post.caption }</div>
             </div>
-            <div>
-                {/* comments display go here */}
+            <div className="comment-display-pc">
+                Comment's go here!
             </div>
-            <div className="" /* comment text area */ >
-                <div className="">
-                    {/* text area */}
-                </div>
-                <button>Post</button>
+            <div className="leave-comment-pc" /* comment text area */ >
             </div>
         </div>
     )

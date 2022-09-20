dbcfe1b5ce43fa81d132fc1b8ba61bd1eef36bc7
david <dwting0322@gmail.com>
2022-09-19 18:02:35 -0700

    fix bugs for redirect to login form

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, useHistory, Redirect } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
@@ -80,8 +80,11 @@ function App() {
       </Switch>
     </BrowserRouter>
   ) :
+    
     <BrowserRouter>
+    <Redirect to='/login' />
       <Switch>
+       
         <Route path='/login' exact={true}>
           <LoginForm />
         </Route>

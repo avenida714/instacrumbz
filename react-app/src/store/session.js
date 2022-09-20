b7ea54208057730999446e7f9233ac749848a7dc
RayC206 <rcv.henry@gmail.com>
2022-09-20 10:55:05 -0700

    working on follow functionality, need to work on user store

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -1,6 +1,8 @@
 // constants
 const SET_USER = 'session/SET_USER';
 const REMOVE_USER = 'session/REMOVE_USER';
+const FOLLOW_USER = 'session/FOLLOW_USER';
+const UNFOLLOW_USER = 'session/UNFOLLOW_USER'
 
 const setUser = (user) => ({
   type: SET_USER,
@@ -9,6 +11,16 @@ const setUser = (user) => ({
 
 const removeUser = () => ({
   type: REMOVE_USER,
+})
+
+const followUser = () => ({
+  type: FOLLOW_USER,
+  user
+})
+
+const unfollowUser = () => ({
+  type: UNFOLLOW_USER,
+  user
 })
 
 const initialState = { user: null };
@@ -97,12 +109,46 @@ export const signUp = (username, email, password) => async (dispatch) => {
   }
 }
 
+export const userFollow = (userId) => async (dispatch) => {
+  const res = await fetch(`/api/followers/follow/${userId}`, {
+    method: 'PUT',
+    headers: { "Content-Type": "application/json" },
+  });
+  if (res.ok){
+    const data = await res.json()
+    dispatch(followUser(data))
+    return data
+  }
+}
+
+export const userUnfollow = (userId) => async (dispatch) => {
+  const res = await fetch(`/api/followers/unfollow/${userId}`, {
+    method: 'PUT',
+    headers: { "Content-Type": "application/json" },
+  });
+  if (res.ok){
+    const data = await res.json()
+    dispatch(followUser(data))
+    return data
+  }
+}
+
 export default function reducer(state = initialState, action) {
+  let newState = {};
   switch (action.type) {
     case SET_USER:
       return { user: action.payload }
     case REMOVE_USER:
       return { user: null }
+    case FOLLOW_USER:
+      newState = {...state}
+      newState.user.followers.push(action.user)
+      return newState
+    case UNFOLLOW_USER:
+      newState = {...state}
+      const index = newState.user.followers.findIndex(user = user.id === action.user.id)
+      newState.user.followers.splice(index,1)
+      return newState
     default:
       return state;
   }

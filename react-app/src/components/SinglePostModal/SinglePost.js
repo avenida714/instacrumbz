52f34588757d9101cc6c435fff758e6162a6377f
Alec Venida <alecvenida@gmail.com>
2022-09-21 16:16:10 -0700

    user info with image in comments; boxes labeled; still need create comment input

--- react-app/src/components/SinglePostModal/SinglePost.js
+++ react-app/src/components/SinglePostModal/SinglePost.js
@@ -1,36 +1,65 @@
-
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { NavLink, useParams } from "react-router-dom";
-import { getOnePostById } from '../../store/posts';
-import EditFormModal from '../EditPostModal';
+import { NavLink, useHistory, useParams } from "react-router-dom";
+import { getOnePostById } from "../../store/posts";
+import EditFormModal from "../EditPostModal";
 
 import "./SinglePost.css";
 
 function SinglePost({ post }) {
   const dispatch = useDispatch();
 
+  const history = useHistory();
+
+  const usersProfilePage = () => {
+    let path = `/profile/${post.owner_id}`;
+    history.push(path);
+  };
 
   if (post) {
     return (
       <div className="outer-most-div-dont-style-me">
 
-          <div className="left-half">
-              <img src={post.image_url} alt="post" className="img" />
-          </div>
+
+        <div className="left-half">
+          <img src={post.image_url} alt="post" className="img" />
+        </div>
 
 
-          <div className="right-half">
-            <div className="post-location">{post.location}</div>
-                <div className="comment-display-pc">
-            {post.comments.map((comment) => (
-              <div key={comment.id}> {comment.comment} </div>
-            ))}
+
+        <div className="right-half">
+
+
+          <div className="right-half-inner">
+            <div className="underline">
+              <div className="header-pc">
+                <div className="user-icon-pc" onClick={usersProfilePage}>
+                  <img
+                    alt="post"
+                    className="img circle"
+                    src={post.user.profile_img}
+                  />
+                </div>
+                <div className="header-info">
+                  <div>{post.user.username}</div>
+                  <div className="gray">{post.location}</div>
+                </div>
+              </div>
+            </div>
+            <div className="caption-comments">
+
+            <div className="comment-display-pc">
+            <div>{post.caption}</div>
+              {post.comments.map((comment) => (
+                <div key={comment.id}> {comment.comment} </div>
+              ))}
+            </div>
+            </div>
+
           </div>
-          <div>{post.caption}</div>
-          <div>{post.user.username}</div>
-          </div>
+        </div>
+
 
       </div>
     );

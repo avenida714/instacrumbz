ac8f4800706bdc32cc55353069cdd32d6773e7c2
RayC206 <rcv.henry@gmail.com>
2022-09-23 14:27:23 -0700

    readded 2 empty files that was breaking site for some reason

---
+++ react-app/src/components/CreatePostModal/CreatePostForm.js
@@ -0,0 +1,17 @@
+
+// import React from 'react';
+// import PostForm from './PostForm';
+
+
+// const CreatePostForm = () => {
+
+//     const post = {
+
+//       };
+
+//     return (
+//       <PostForm post={post} formType="Create Post" />
+//     );
+//   }
+
+//   export default CreatePostForm;
---
+++ react-app/src/components/CreatePostModal/EditPostForm.js
@@ -0,0 +1,20 @@
+
+// import React from 'react';
+// import { NavLink, useParams } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+// import PostForm from './PostForm';
+
+
+// const EditPostForm = () => {
+//     const {postId} = useParams();
+//     console.log("postId from EditPost***********", postId)
+
+//     const post = useSelector(state => state.post[postId])
+//     console.log("post from EditPost***************", post)
+
+//     return (
+//         <PostForm post={post} formType="Update Post" />
+//       );
+//     }
+
+//     export default EditPostForm;
